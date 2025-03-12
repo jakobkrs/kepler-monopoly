eb8bf9ef7bb3ce4068cb4319887f92fa8f3e3685
Lorenz993 <lorenzmehner@gmx.de>
2025-03-12 12:03:31 +0100

    Klasse Square implementiert

---
+++ square.py
@@ -0,0 +1,6 @@
+class Square():
+    """Klasse Square"""
+    def __init__(self, name, position, squaretype)
+        self.__name = name
+        self.__position = position
+        self.__type = squaretype
