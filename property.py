92705ad8bafabd45b84894e2dbb48eedd5ca2c98
2far-Apart <hermann.neuberg@gmail.com>
2025-03-13 14:20:12 +0100

    Update property.py

--- property.py
+++ property.py
@@ -1,1 +1,20 @@
 
+class Property():
+    def__init__(self,name,houses,rent,cost,mortgage,owner):
+    """
+    Initalisation and determination of value
+    """
+    self.__name=name
+    self.__häuser=houses
+    self.__miete=rent
+    self.__grundstückspreis=cost
+    self.__hypothek=mortgage
+    self.__besitzer=owner
+
+    def setRent(self):
+        """
+        Mietpreis eines Grundstücks festlegen
+        """
+        
+
+
