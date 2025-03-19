2f8da66c30e4e519f4f3a12dc1bf63d534fef948
Elif14916 <158343216+Elif14916@users.noreply.github.com>
2025-03-19 18:20:48 +0100

    Methode nextPlayerTurn() hinzugefügt um einen Spielerloop zu haben

--- game.py
+++ game.py
@@ -1,32 +1,51 @@
+from player import *
+from square import *
+
 class Game():
     def __init__(self):
         """
         Initialisation of the Game class. Set start values of class instance.
         """
-        self.__currentPlayer = 0
+        self.__currentPlayerId = 0
+        self.__currentPlayer = None
         self.__playerCount = 0
-        self.__playerOrder = []             # add shuffling mechanic !later
-        self.__players = []                 # list of object from class Player
+        self.__playerOrder = []             # Füge die Reihenfolgebestimmung hinzu !later
+        self.__players = []                 # Liste von Objekten der Klasse Spieler
         self.__gameBoard = []
-        self.__communityCrads = []
+        self.__communityCards = []
         self.__eventCards = []
 
         
 
     
 
-    def __addPlayer(self, player: Player):
+    def addPlayer(self, player: Player):
+        """
+        Fügt einen Spieler zur Liste aller Spieler hinzu.
+        """
         self.__players.append(player)
         self.__playerCount += 1
         self.__playerOrder.append(self.__playerCount - 1)
 
-    def __shufflePlayerOrder(self): #WIP !later
+    def shufflePlayerOrder(self): #WIP !later
         pass
 
-    def __loadCSV():    #added !later by other teammember
+    def __loadCSV():    # wird später von anderem Teammitglied erstellt !later
+        return [[]]
+
+    def trade(self,player1: Player,player2: Player):
         pass
 
-    def __trade(self):
-        pass
+    def nextPlayersTurn(self):
+        """
+        Der nächste Spieler kommt zum Zug.
+        """
+        self.__currentPlayerId = (self.__currentPlayerId + 1) % self.__playerCount
+        self.__currentPlayer = self.__players[self.__playerOrder[self.__currentPlayerId]]
 
+        self.__currentPlayer.startTurn()
+
+
+
+game = Game()
 
