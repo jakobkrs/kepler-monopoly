9de1c830ea84f7b31dbc34d87ac9fb7c56bfadf9
Lorenz993 <lorenzmehner@gmx.de>
2025-03-27 14:01:00 +0100

    loadCSV.py gelöscht und in game.py copiert

--- game.py
+++ game.py
@@ -30,8 +30,23 @@ class Game():
     def shufflePlayerOrder(self): #WIP !later
         pass
 
-    def __loadCSV():    # wird später von anderem Teammitglied erstellt !later
-        return [[]]
+    def __loadCSV(filepath):    # wird später von anderem Teammitglied erstellt !later
+        """
+        Methode, um Square aus der CSV-Datei einzulesen
+        """
+        table =[]
+        file = open(filepath,"r")                              # Datei öffnen zum Lesen
+        start = True                                                            # Variable, um erste Zeile ignorieren zu können
+        for line in file:
+            if start:                                                           # erste Zeile ignorieren
+                start = not(start)
+            else:
+                a = line.rstrip().split(";") 
+                table.append(a)                                  # Zeile in Feld auftrennen
+
+        file.close
+        return table
+        # return [[]]
 
     def trade(self,player1: Player,player2: Player):
         pass
