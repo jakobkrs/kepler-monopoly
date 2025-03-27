62e9747c14309180a3cd2441ae695b6a055d111a
2far-Apart <hermann.neuberg@gmail.com>
2025-03-27 14:18:04 +0100

    Implementierung der buildHouse, getHouseCount und setOwner Prozedur. Muss auf funktionalität geprüft werden.

--- property.py
+++ property.py
@@ -30,4 +30,21 @@ class Property():
             return 0 #IDK WASS HIER HIN SOLL!!! #Haus kann nicht gebaut werden da dieses Grundstück noch keinen besitzer hat.
 
         if self.__houses >= 5 #4 Häuser + 1 Hotel als Maximum
+            return 0
+
+        self.__houses +=1
+        #print("Ein Haus wurde auf " + self.name + " gebaut. Neue Anzahl: self.__houses")
+
+    def getHouseCount(self):
+        """
+        Prozedur zum abrufen der Anzahl von Häusern auf einem Grundstück
+        """
+        return self.__houses
+
+    def setOwner(self, owner):
+        """
+        Stellt einen neuen Besitzer für das Grundstück fest
+        """
+        self.__owner = owner
+    
 
