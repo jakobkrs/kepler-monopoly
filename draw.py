71313e64a5987bd8e5fb8852ef1a41d8ecf4913f
Jakob Krause <96916948+jakobkrs@users.noreply.github.com>
2025-03-27 14:13:00 +0100

    spielfeld mit testbild skalierbar

--- draw.py
+++ draw.py
@@ -2,37 +2,52 @@ import pygame
 
 # pygame initialisieren
 pygame.init()
+boardImage = pygame.image.load("board.png")
 
-# Fenster
-screenWidth, screenHeight = pygame.display.get_desktop_sizes()[0]
-print(screenWidth, screenHeight)
+# Standardfenstergröße setzen
+screenWidth, screenHeight = 1280, 720  # Sichere Standardwerte
 screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.RESIZABLE)
 pygame.display.set_caption("KeplerMonopoly")
 
+# Farben
 white = (255, 255, 255)
 black = (0, 0, 0)
 lightGray = (200, 200, 200)
 
-margin = min(screenWidth, screenHeight) // 20  # 5% Rand
-boardWidth = screenWidth // 2 - margin
-boardHeight = boardWidth
-print(boardWidth)
-boardX = margin  # Linker Rand
-boardY = (screenHeight - boardHeight) / 2 # Oberer Rand
+# Spielfeldgrößen berechnen
+def recalculateSizes():
+    global margin, boardWidth, boardHeight, boardX, boardY, boardImage, scaledBoardImage
+    margin = min(screenWidth, screenHeight) // 20  # 5% Rand
+    boardHeight = screenHeight - margin
+    boardWidth = screenWidth // 2 - margin
+    if boardHeight > boardWidth:
+        boardHeight = boardWidth
+    else:
+        boardWidth = boardHeight
+    boardX = margin  # Linker Rand
+    boardY = (screenHeight - boardHeight) / 2  # Oberer Rand
 
+    scaledBoardImage = pygame.transform.scale(boardImage, (boardHeight, boardWidth))
 
+# Initiale Berechnung der Spielfeldgrößen
+recalculateSizes()
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-    screen.fill(white)
+        elif event.type == pygame.VIDEORESIZE:
+            screenWidth, screenHeight = event.size  # Neue Fenstergröße speichern
+            screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.RESIZABLE)  # Fenster neu setzen
+            recalculateSizes()  # Alle Werte neu berechnen
     
+    screen.fill(white)
+
     # Spielfeld zeichnen
     pygame.draw.rect(screen, lightGray, (boardX, boardY, boardWidth, boardHeight))
-    pygame.draw.rect(screen, black, (0,0,100,100))
+    screen.blit(scaledBoardImage, (boardX, boardY))
+
     pygame.display.update()
 
 pygame.quit()
