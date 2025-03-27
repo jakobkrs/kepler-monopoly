efe32164afe27f61c1992b2a8b9c914d6ef6b3a5
Jakob Krause <96916948+jakobkrs@users.noreply.github.com>
2025-03-27 13:26:08 +0100

    update

--- draw.py
+++ draw.py
@@ -1,22 +1,38 @@
 import pygame
 
-# Initialize the game
+# pygame initialisieren
 pygame.init()
 
-# Set up the display
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+# Fenster
+screenWidth, screenHeight = pygame.display.get_desktop_sizes()[0]
+print(screenWidth, screenHeight)
+screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.RESIZABLE)
+pygame.display.set_caption("KeplerMonopoly")
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+lightGray = (200, 200, 200)
+
+margin = min(screenWidth, screenHeight) // 20  # 5% Rand
+boardWidth = screenWidth // 2 - margin
+boardHeight = boardWidth
+print(boardWidth)
+boardX = margin  # Linker Rand
+boardY = (screenHeight - boardHeight) / 2 # Oberer Rand
+
+
 
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             
-    screen.fill((255, 255, 255))
+    screen.fill(white)
+    
+    # Spielfeld zeichnen
+    pygame.draw.rect(screen, lightGray, (boardX, boardY, boardWidth, boardHeight))
+    pygame.draw.rect(screen, black, (0,0,100,100))
     pygame.display.update()
 
-            
 pygame.quit()
