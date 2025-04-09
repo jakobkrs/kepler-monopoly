0a09b4c9ab965793d0c3820e9619329854524510
Jakob Krause <96916948+jakobkrs@users.noreply.github.com>
2025-04-09 11:30:30 +0200

    Anpassung square für draw

--- square.py
+++ square.py
@@ -1,8 +1,34 @@
 class Square():
-    def __init__(self, name, position, squaretype)
+    def __init__(self, name, position, squaretype):
         """
         Konstruktor der Klasse Square
         """
         self.__name = name
         self.__position = position
         self.__type = squaretype
+        
+    def setFieldCoord(self, x, y, width, height):
+        """
+        Setzt die Position und Maße des Feldes
+        """
+        self.__fieldX = x
+        self.__fieldY = y
+        self.__fieldWidth = width
+        self.__fieldHeight = height
+        
+    def getFieldCoord(self, whichInfo):
+        """
+        Gibt die Position und Maße des Feldes zurück
+        """
+        if whichInfo == "x":
+            return self.__fieldX
+        elif whichInfo == "y":
+            return self.__fieldY
+        elif whichInfo == "width":
+            return self.__fieldWidth
+        elif whichInfo == "height":
+            return self.__fieldHeight
+        
+        
+        
+        
